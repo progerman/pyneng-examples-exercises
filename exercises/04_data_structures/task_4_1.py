620338cf446b4918eaa1a5d095435268b1290e4f
alex <aleksandr.telenchenko@gmail.com>
2021-02-14 10:55:14 -0600

    add task exec 4.1 4.4 4.5

--- exercises/04_data_structures/task_4_1.py
+++ exercises/04_data_structures/task_4_1.py
@@ -20,3 +20,4 @@ nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
 nat=nat.replace('FastEthernet','GigabitEthernet')
 print(nat)
 
+
--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -22,3 +22,9 @@
 
 command1 = "switchport trunk allowed vlan 1,2,3,5,8"
 command2 = "switchport trunk allowed vlan 1,3,8,9"
+
+command1=set(command1.split()[-1].split(','))  
+command2=set(command2.split()[-1].split(',')) 
+result=list(command1.intersection(command2))
+result.sort()
+print(result)
