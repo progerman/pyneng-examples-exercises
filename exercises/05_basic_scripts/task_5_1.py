e0791d37e495653d155c6641926b193a8ddbf2bf
alex <aleksandr.telenchenko@gmail.com>
2021-02-21 08:52:57 -0600

    exec task 5.1

--- exercises/05_basic_scripts/task_5_1.py
+++ exercises/05_basic_scripts/task_5_1.py
@@ -21,6 +21,9 @@ $ python task_5_1.py
 решить без использования условия if.
 """
 
+
+
+
 london_co = {
     "r1": {
         "location": "21 New Globe Walk",
@@ -46,3 +49,12 @@ london_co = {
         "routing": True,
     },
 }
+
+dev_name=input('Введите имя устройства:')
+print(london_co[dev_name])
+
+
+
+
+
+
