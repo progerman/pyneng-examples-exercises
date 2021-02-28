a238216956c5aadd51c4e40c05410f261d0bf110
alex <aleksandr.telenchenko@gmail.com>
2021-02-28 14:01:21 -0600

    exex task_7_2b.py test not  , run ok

--- exercises/07_files/task_7_2b.py
+++ exercises/07_files/task_7_2b.py
@@ -17,3 +17,16 @@
 """
 
 ignore = ["duplex", "alias", "configuration"]
+
+from sys import argv
+
+name_file=argv
+if name_file[1::] != 0:
+    with open(''.join(name_file[1]), 'r') as doc_1 , open(''.join(name_file[2]), 'w') as dest_doc: #python task_7_2b.py config_sw1.txt config.txt
+        for list1 in doc_1:
+            for ignore_list in ignore:
+                if list1.startswith('!') == True: break
+                elif list1.find(ignore_list) != -1: break
+                elif list1 == '\n' : break
+            else:dest_doc.write(list1)
+else:print('enter file name')
