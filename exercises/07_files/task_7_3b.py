c99d5ed08c594d48ff7c343b359860b4e6407e8f
alex <aleksandr.telenchenko@gmail.com>
2021-03-04 04:56:46 -0600

    exec task_7_3b.py  run ok , test not

--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -17,3 +17,25 @@ Enter VLAN number: 10
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+vlan_id_input=input('input vlan ID: ')
+vlan_list = []
+list2 = []
+ff = []
+kk = []
+with open('CAM_table.txt', 'r') as doc_1:
+    for list1 in doc_1:
+        list2.append(list1.split('\n'))
+    for i in list2:
+        for aa in i:
+            indx1 = aa.strip().replace('DYNAMIC', '')
+            if indx1[0:5].strip().isdigit() == True :
+                vlan_list.append(int(indx1[0:5]))
+                vlan_list.append(indx1)
+    for ff in range(0,len(vlan_list), 2):
+        kk.append(vlan_list[ff:ff+2])
+        kk.sort()
+    for n in kk:     
+        if n[0] == int(vlan_id_input):
+            print(n[1])
+        
