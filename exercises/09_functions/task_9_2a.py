484ad9d276f24de9fa7b809e5d3e86c70ab21749
alex <aleksandr.telenchenko@gmail.com>
2021-03-08 04:41:07 -0600

    exec task_9_2a.py run ok test ok

--- exercises/09_functions/task_9_2a.py
+++ exercises/09_functions/task_9_2a.py
@@ -27,3 +27,62 @@ trunk_config = {
     "FastEthernet0/2": [11, 30],
     "FastEthernet0/4": [17],
 }
+
+
+def generate_trunk_config(intf_vlan_mapping,trunk_template):
+    
+    
+    result_config_dict={}
+    
+    for interface, vlans in intf_vlan_mapping.items():
+        vlan_id_result=[]
+        result_config_list=[]
+        for vlan_id in vlans:
+            vlan_id_result.append(str(vlan_id))
+        for config_line in trunk_template:
+            if config_line.endswith('allowed vlan'):
+                a = config_line + ' ' + ','.join(vlan_id_result)
+                result_config_list.append(a.strip())
+            else:
+                result_config_list.append(config_line.strip())
+                
+            result_config_dict[interface]=result_config_list
+        
+    return result_config_dict
+           
+get_config=generate_trunk_config(trunk_config,trunk_mode_template)
+
+#print(get_config)
+
+for line2 in get_config.items():
+    print(line2[0])
+    
+    for line3 in line2[1]:
+        print(line3)
+       
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
