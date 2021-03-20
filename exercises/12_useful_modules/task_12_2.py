48861f4c8bcaaafa8075cfced2697f0130275979
alex <aleksandr.telenchenko@gmail.com>
2021-03-20 15:27:40 -0500

    exec task_12_2.py test ok run ok

--- exercises/12_useful_modules/task_12_2.py
+++ exercises/12_useful_modules/task_12_2.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import ipaddress
+from pprint import pprint
 """
 Задание 12.2
 
@@ -34,3 +36,62 @@
  '172.21.41.129', '172.21.41.130', '172.21.41.131', '172.21.41.132']
 
 """
+
+test_ip_list=['8.8.4.4', '1.1.1.1-3', '172.21.41.128-172.21.41.132', '10.1.1.1-345','192.168.13.a-10']
+
+
+
+
+
+
+
+def convert_ranges_to_ip_list(ip_adreses):
+    result_ip_renge=[]
+    intermediate_ip_range=[]
+    for one_ip_adress in ip_adreses:
+        try:
+            if ipaddress.IPv4Address(one_ip_adress).version  == 4 :
+                result_ip_renge.append(one_ip_adress)
+            
+        
+        except:
+            
+            intermediate_ip_range.append(one_ip_adress)
+            
+    for ip in  intermediate_ip_range:       
+        if '-' in one_ip_adress :
+            ips=ip.split('-')
+            try:
+                if (ipaddress.IPv4Address(ips[0]).version  == 4 and 
+                    ipaddress.IPv4Address(ips[1]).version  == 4 ):
+                    gain_ip=int(ips[1].split('.')[-1])-int(ips[0].split('.')[-1])
+                    for i in range(gain_ip+1):
+                        result_ip_renge.append(str(ipaddress.IPv4Address(ips[0])+i))
+           
+                    
+            except:
+                try:
+                    if ipaddress.IPv4Address(ips[0]).version  == 4 and 0 < int(ips[1]) < 255:
+                        gain_ip = int(ips[1])-int(ips[0].split('.')[-1])
+                        for i in range(gain_ip+1):
+                            result_ip_renge.append(str(ipaddress.IPv4Address(ips[0])+i))
+                    else:print('false ip adress',ip )
+                    
+                except: print('false ip adress',ip )
+        
+        
+    
+    return result_ip_renge
+
+
+
+pprint(convert_ranges_to_ip_list(test_ip_list))
+
+
+
+
+
+
+
+
+
