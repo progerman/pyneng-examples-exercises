08e16b009f7bfa6ddba56a20fd4d20228c1677d3
alex <aleksandr.telenchenko@gmail.com>
2021-03-28 12:30:18 -0500

    exec task_15_1a.py test ok run ok

--- exercises/15_module_re/task_15_1a.py
+++ exercises/15_module_re/task_15_1a.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 """
 Задание 15.1a
 
@@ -24,3 +25,26 @@
 а не ввод пользователя.
 
 """
+
+
+
+def get_ip_from_cfg(file_name):
+    with open(file_name) as data:
+        regex_intf=(r'(interface.)(\S+\d+)' )
+        regex=(r'(.ip address.)(\S+.\S+.\S+.\S+) +(\S+.\S+.\S+.\S+)')
+        result_dict={}
+        result_list=[]
+        result_tupl=()
+        for line in data:
+            match = re.search(regex, line)
+            match1 = re.search(regex_intf,line)
+            if match1:
+                a=match1.group(2)
+            if match:
+                result_tupl=(match.group(2),match.group(3))
+                result_dict[a]=result_tupl
+    return result_dict
+    
+    
+dict1=get_ip_from_cfg('config_r1.txt')
+print(dict1)
