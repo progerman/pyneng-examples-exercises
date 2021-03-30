6bf539bcd1e28b899aa6c840891ef1e9dc160f3c
alex <aleksandr.telenchenko@gmail.com>
2021-03-30 00:31:33 -0500

    exec task_15_2.py run ok test ok

--- exercises/15_module_re/task_15_2.py
+++ exercises/15_module_re/task_15_2.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+
+from pprint import pprint
+import re
 """
 Задание 15.2
 
@@ -21,3 +24,32 @@
 Проверить работу функции на примере файла sh_ip_int_br.txt.
 
 """
+
+
+
+def parse_sh_ip_int_br(file_name):
+    with open(file_name) as data:
+        regex1=(r'(\S+\d+)\s+(\S+)\s+(?:YES)\s(?:\S+)\s+(\S+)\s+(\S+)')
+        regex2=(r'(\S+\d+)\s+(\S+)\s+(?:YES)\s(?:\S+)\s+(\S+\s+\S+)\s+(\S+)')
+        result_tupl1=()
+        result_tupl2=()
+        result_list=[]
+        
+        for line in data:
+            if not 'administratively down' in line: 
+                match=re.search(regex1,line)
+                if match:
+                    result_tupl1=(match.group(1),match.group(2),match.group(3),match.group(4))
+                    result_list.append(result_tupl1)
+            else:
+                match2=re.search(regex2,line)
+                if match2:
+                    result_tupl2=(match2.group(1),match2.group(2),match2.group(3),match2.group(4))
+                    result_list.append(result_tupl2)
+                    
+    return result_list
+    
+    
+forprint=parse_sh_ip_int_br('sh_ip_int_br.txt')
+
+pprint(forprint)
