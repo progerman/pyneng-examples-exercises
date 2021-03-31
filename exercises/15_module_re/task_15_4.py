15b90cda619cdc896f592274a6fbd1642a9718c7
alex <aleksandr.telenchenko@gmail.com>
2021-03-31 13:40:21 -0500

    exec task_15_4.py test ok run ok

--- exercises/15_module_re/task_15_4.py
+++ exercises/15_module_re/task_15_4.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import re
+from pprint import pprint
 """
 Задание 15.4
 
@@ -22,3 +24,57 @@ interface Loopback0
 
 Проверить работу функции на примере файла config_r1.txt.
 """
+
+
+
+def get_ints_without_description(config_file_name):
+    with open(config_file_name) as data:
+        a = 0
+        b = ''
+        result_list = []
+        regex=(r'\S+\s+(\S+)')
+        for num ,i in  enumerate(data):
+            if  i.startswith('interface'): 
+                a = num
+                b = re.search(regex,i).group(1)
+            elif i.startswith(' description') == False and (num == a+1 and b != ''):
+                result_list.append(b.rstrip())
+    return result_list
+            
+            
+            
+    
+    
+
+
+for_print=get_ints_without_description('config_r1.txt')
+
+
+pprint(for_print)
+
+'''
+#Вариант Наташи
+
+
+import re
+
+
+def get_ints_without_description(config):
+    regex = re.compile(r"!\ninterface (?P<intf>\S+)\n"
+                       r"(?P<descr> description \S+)?")
+    with open(config) as src:
+        match = regex.finditer(src.read())
+        result = [m.group('intf') for m in match if m.lastgroup == 'intf']
+        return result
+'''
+
+
+
+
+
+
+
+
+
+
+
