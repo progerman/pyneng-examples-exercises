8411176c81003461cab310d94ee964bfbbeab326
alex <aleksandr.telenchenko@gmail.com>
2021-04-01 05:28:23 -0500

    exec task_15_5.py test ok run ok

--- exercises/15_module_re/task_15_5.py
+++ exercises/15_module_re/task_15_5.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import re
+from pprint import pprint
+
 """
 Задание 15.5
 
@@ -26,3 +29,21 @@ description Connected to SW1 port Eth 0/1
 
 Проверить работу функции на файле sh_cdp_n_sw1.txt.
 """
+
+
+def generate_description_from_cdp(file_name):
+    with open(file_name) as data:
+        regex=(r'(?P<dev>\S+\d+)\s+(?P<local_port>\S+\s+\S+).+ (?P<dest_port>\S+ \S+)')
+        result_dict={}
+        for line in data:
+            match=re.search(regex,line)
+            if match:
+                dev=match.group('dev')
+                local_port = match.group('local_port')
+                dest_port = match.group('dest_port')
+                result_dict[local_port]=f'description Connected to {dev} port {dest_port}'
+    return result_dict
+            
+            
+pprint(generate_description_from_cdp('sh_cdp_n_sw1.txt'))
+    
