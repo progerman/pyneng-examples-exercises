2c23b0d26c8f1a35c5fabccc252dec39a75854e4
alex <aleksandr.telenchenko@gmail.com>
2021-04-05 08:07:22 -0500

    exec task_17_1.py run ok test ok

--- exercises/17_serialization/task_17_1.py
+++ exercises/17_serialization/task_17_1.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+
+import re
+from pprint import pprint
+import csv
 """
 Задание 17.1
 
@@ -31,3 +35,58 @@ sw3,00:E9:22:11:A6:50,100.1.1.7,3,FastEthernet0/21
 sw2_dhcp_snooping.txt, sw3_dhcp_snooping.txt.
 
 """
+
+filenames = ['sw1_dhcp_snooping.txt','sw2_dhcp_snooping.txt', 'sw3_dhcp_snooping.txt']
+output ='result.csv'
+
+def write_dhcp_snooping_to_csv(config_list,output):
+        regex=(r'((?:\S+:){5}\S+)\s+(\d+\.\d+\.\d+\.\d+)\s+\d+\s+\S+\s+(\d+)\s+(\S+)')
+        header=['switch','mac','ip','vlan','interface']
+        regex_name_dev=(r'(\S+\d)\_')
+        result_list=[header]
+        
+        for member_list in config_list:
+            with open(member_list) as f:
+                net_dev = []
+                for i in f:
+                    match=re.search(regex,i)
+                    match_name_dev=re.search(regex_name_dev,member_list)
+                    if match_name_dev and match:
+                       result_list.append(list(match_name_dev.groups(0) + match.groups(0)))
+                        
+        with open(output, 'w') as f1:
+            writer = csv.writer(f1)
+            for row in result_list:
+                writer.writerow(row)
+                
+
+write_dhcp_snooping_to_csv(filenames,output)
+
+'''
+#Вариант Наташи
+
+
+
+import csv
+import re
+import glob
+
+
+def write_dhcp_snooping_to_csv(filenames, output):
+    regex = r"(\S+) +(\S+) +\d+ +\S+ +(\d+) +(\S+)"
+    with open(output, "w") as dest:
+        writer = csv.writer(dest)
+        writer.writerow(["switch", "mac", "ip", "vlan", "interface"])
+        for filename in filenames:
+            switch = re.search("([^/]+)_dhcp_snooping.txt", filename).group(1)
+            with open(filename) as f:
+                for line in f:
+                    match = re.search(regex, line)
+                    if match:
+                        writer.writerow((switch,) + match.groups())
+
+
+if __name__ == "__main__":
+    sh_dhcp_snoop_files = glob.glob("*_dhcp_snooping.txt")
+    write_dhcp_snooping_to_csv(sh_dhcp_snoop_files, "example_csv.csv")
+'''
