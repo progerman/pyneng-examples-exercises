e3dc780f57618c7ef4d462dda669840ad7ec7a31
alex <aleksandr.telenchenko@gmail.com>
2021-05-03 12:52:43 -0500

    exec task_18_2c.py run ok , test not

--- exercises/18_ssh_telnet/task_18_2c.py
+++ exercises/18_ssh_telnet/task_18_2c.py
@@ -54,4 +54,87 @@ In [12]: pprint(result)
 commands_with_errors = ["logging 0255.255.1", "logging", "a"]
 correct_commands = ["logging buffered 20010", "ip http server"]
 
-commands = commands_with_errors + correct_commands
+commands = correct_commands + commands_with_errors 
+
+
+
+
+import yaml
+import netmiko
+from pprint import pprint
+from netmiko import (
+    ConnectHandler,
+    NetmikoTimeoutException,
+    NetmikoAuthenticationException,
+)
+
+
+def send_config_commands(dev, commands, error_case, log=True):
+    try:
+        question=True
+        with ConnectHandler(**dev) as ssh:
+            if log:print('-'*50 , f' , Подключаюсь к {list(dev.values())[1]}')
+            ssh.enable()
+            result_send_command_ok = {}
+            result_send_command_erro = {}
+            result = []
+            result1 = []
+            result2 = []
+            
+            for command in commands:
+                output = ssh.send_config_set(command)
+                
+                for err_str in error_case :
+                    if err_str in output and question:
+                        print(f'Команда {command} выполнилась с ошибкой {err_str} на устройстве {list(dev.values())[1]}')
+                        result_send_command_erro[command]=output
+                        result2.append(result_send_command_erro)
+                        question = input('Продолжать выполнять команды? [y]/n: ')
+                        if question == ('n' or 'no') :
+                            question=False
+                            print('Выполнение команд остановлено')
+                        else:question=True
+                        break
+                if result_send_command_erro.get(command) == None and question:
+                    result_send_command_ok[command]=output
+                    result1.append(result_send_command_ok)
+                    print(f'Команда {command} выполнилась успешно на устройстве {list(dev.values())[1]}')
+                
+                #print('Выполнение команд остановлено')
+                #break
+        return result , result1 , result2
+        
+        
+         
+    except (NetmikoTimeoutException, NetmikoAuthenticationException) as error:
+        print(error)
+    
+
+if __name__ == "__main__":
+    
+    error_case=['Invalid input detected', 'Incomplete command', 'Ambiguous command']
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+    for dev in devices:
+        res=send_config_commands(dev, commands, error_case)
+        #for i in res[0]:
+            #print (i)
+        for j in res[1]:
+            pprint(j)
+            break
+        for m in res[2]:
+            pprint(m)
+            break
+
+
+
+
+
+
+
+
+
+
+
+
+
