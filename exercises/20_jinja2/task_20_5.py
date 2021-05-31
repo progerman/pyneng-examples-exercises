a8f02784e00134488f9f734d83cfb15e7a24a40a
alex <aleksandr.telenchenko@gmail.com>
2021-05-31 07:56:30 -0500

    exec task_20_5.py test ok

--- exercises/20_jinja2/task_20_5.py
+++ exercises/20_jinja2/task_20_5.py
@@ -27,6 +27,9 @@ cisco_vpn_1.txt и cisco_vpn_2.txt.
 Примеры конфигураций VPN, которые должна возвращать функция create_vpn_config в файлах
 cisco_vpn_1.txt и cisco_vpn_2.txt.
 """
+import yaml
+from task_20_1 import generate_config
+
 
 data = {
     "tun_num": 10,
@@ -35,3 +38,79 @@ data = {
     "tun_ip_1": "10.0.1.1 255.255.255.252",
     "tun_ip_2": "10.0.1.2 255.255.255.252",
 }
+
+
+
+def create_vpn_config(template1, template2, data_dict):
+    config_vpn_1=generate_config(template1,data_dict)
+    config_vpn_2=generate_config(template2,data_dict)
+    print(config_vpn_1)
+    print('-'*100)
+    print(config_vpn_2)
+    print('-'*100)
+    return config_vpn_1 , config_vpn_2
+    
+    
+    
+if __name__ == "__main__":
+    
+    template1 = "templates/gre_ipsec_vpn_1.txt"
+    template2 = "templates/gre_ipsec_vpn_2.txt"
+    print(create_vpn_config(template1, template2, data))
+
+
+
+
+
+'''
+templates/gre_ipsec_vpn_1.txt
+
+
+crypto isakmp policy 10
+ encr aes
+ authentication pre-share
+ group 5
+ hash sha
+
+crypto isakmp key cisco address {{wan_ip_1}}
+
+crypto ipsec transform-set AESSHA esp-aes esp-sha-hmac
+ mode transport
+
+crypto ipsec profile GRE
+ set transform-set AESSHA
+
+interface Tunnel {{tun_num}}
+ ip address {{tun_ip_1}}
+ tunnel source  {{wan_ip_1}}
+ tunnel destination  {{wan_ip_2}}
+ tunnel protection ipsec profile GRE
+ 
+ --------------------------------------------------
+ "templates/gre_ipsec_vpn_2.txt"
+ 
+ 
+ crypto isakmp policy 10
+ encr aes
+ authentication pre-share
+ group 5
+ hash sha
+
+crypto isakmp key cisco address {{wan_ip_2}}
+
+crypto ipsec transform-set AESSHA esp-aes esp-sha-hmac
+ mode transport
+
+crypto ipsec profile GRE
+ set transform-set AESSHA
+
+interface Tunnel {{tun_num}}
+ ip address {{tun_ip_2}}
+ tunnel source  {{wan_ip_2}}
+ tunnel destination  {{wan_ip_1}}
+ tunnel protection ipsec profile GRE
+
+'''
+ 
+
+
