0e73c95038a56d9cacf077ff47ac0bd4eb7733b0
alex <aleksandr.telenchenko@gmail.com>
2021-06-12 11:46:16 -0500

    exec task_21_4.py test not , run ok

--- exercises/21_textfsm/task_21_4.py
+++ exercises/21_textfsm/task_21_4.py
@@ -21,3 +21,30 @@
 Проверить работу функции на примере вывода команды sh ip int br
 и устройствах из devices.yaml.
 """
+from task_21_3 import parse_command_dynamic
+from textfsm import clitable
+from pprint import pprint
+from task_18_1 import send_show_command
+
+def send_and_parse_show_command(device_dict, command, templates_path, index, attributes):
+    show_command_result = send_show_command(device_dict, command)
+    parse_result = parse_command_dynamic(show_command_result, attributes, index, templates_path ) 
+    return parse_result
+    
+
+
+
+if __name__ == "__main__":
+    device = {
+            "device_type": "cisco_ios_telnet",
+            "host": "192.168.100.1",
+            "username": "cisco",
+            "password": "cisco",
+            "secret": "cisco",
+            }
+    command = "sh ip int br"
+    attributes = {'Command' : 'sh ip int br', 'Vendor' : 'cisco_ios'}
+    index_file ='index'
+    templ_path ="templates"
+    result = send_and_parse_show_command(device, command, templ_path , index_file , attributes)
+    pprint(result)
