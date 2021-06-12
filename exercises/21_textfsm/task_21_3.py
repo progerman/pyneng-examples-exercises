f09f13f38becc9ddae53d4449e1f2149c327ac58
alex <aleksandr.telenchenko@gmail.com>
2021-06-12 05:23:32 -0500

    exec task_21_3.py test not , run ok

--- exercises/21_textfsm/task_21_3.py
+++ exercises/21_textfsm/task_21_3.py
@@ -20,3 +20,30 @@
 
 Проверить работу функции на примере вывода команды sh ip int br.
 """
+
+from task_21_1a import parse_output_to_dict
+from textfsm import clitable
+from pprint import pprint
+
+
+def parse_command_dynamic(command_output, attributes_dict, index_file, templ_path):
+    cli_table = clitable.CliTable(index_file, templ_path)
+    cli_table.ParseCmd(command_output, attributes_dict)
+    result_dict={}
+    parse_result_list=[]
+    for j in cli_table:
+        result_dict = dict(zip(cli_table.header, j))
+        parse_result_list.append(result_dict)
+    return parse_result_list
+    
+
+
+if __name__ == "__main__":
+    with open("output/sh_cdp_n_det.txt", 'r') as f:
+        output = f.read()
+        attributes = {'Command' : 'sh cdp ne det', 'Vendor' : 'cisco_ios'}
+        index_file='index'
+        templ_path="templates"
+        result=parse_command_dynamic(output, attributes, index_file, templ_path)
+        pprint(result)
+ 
