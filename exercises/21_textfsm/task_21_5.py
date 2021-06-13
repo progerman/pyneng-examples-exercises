0473f5219b3b4cb387129bc837ff1bedaf749d07
alex <aleksandr.telenchenko@gmail.com>
2021-06-13 05:13:00 -0500

    exec task_21_5.py test not , run ok

--- exercises/21_textfsm/task_21_5.py
+++ exercises/21_textfsm/task_21_5.py
@@ -38,3 +38,70 @@
 Проверить работу функции на примере вывода команды sh ip int br
 и устройствах из devices.yaml.
 """
+import yaml
+from netmiko import ConnectHandler
+from pprint import pprint
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from itertools import repeat
+import textfsm
+
+
+def send_show(devices, command):
+    with ConnectHandler(**devices) as ssh:
+        list_result = []
+        ssh.enable()
+        result = ssh.send_command(command,strip_command=True, strip_prompt=True)
+        for i in result.split('\n'):
+            list_result.append(i)
+            list_result.append(devices['host'])
+    return list_result 
+
+
+def send_show_command_to_devices(devices, command,  limit = 3):
+    with ThreadPoolExecutor(max_workers = limit) as executor:
+        list_1 = []
+        result = executor.map(send_show, devices, repeat(command))
+        for i in result:
+            list_1.append(i)
+        return list_1
+    
+
+def parse_output_to_dict(template, command_output):
+    with open(template) as template:
+        fsm = textfsm.TextFSM(template)
+        parse_result = fsm.ParseText(command_output)
+        result_dict = {}
+        for j in parse_result:
+            result_dict = dict(zip(fsm.header, j))
+        return result_dict
+    
+    
+def send_and_parse_command_parallel(devices, command, templates_path, limit = 3):
+    result_dict = {}
+    for output_list in send_show_command_to_devices(devices, command, limit):
+        parse_result_list=[]
+        for output in output_list[::-1]:
+            dict_parse_command = parse_output_to_dict(templates_path, output)
+            if dict_parse_command != {}:
+                parse_result_list.append(dict_parse_command)
+            result_dict[output_list[-1]] = parse_result_list
+    return result_dict
+        
+    
+if __name__ == '__main__':
+    with open('devices.yaml') as f:
+        devices = yaml.safe_load(f)
+    command = "sh ip int br"
+    result = send_and_parse_command_parallel(devices, command, "templates/sh_ip_int_br.template" )
+    pprint(result)
+    
+    
+    
+
+
+
+
+
+
+
+
