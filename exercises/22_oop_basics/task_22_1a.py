ba685201ba575e74e011b2e91d7accef24891c8f
alex <aleksandr.telenchenko@gmail.com>
2021-06-15 09:01:27 -0500

    task_22_1a.py test ok v2

--- exercises/22_oop_basics/task_22_1a.py
+++ exercises/22_oop_basics/task_22_1a.py
@@ -22,31 +22,26 @@ topology_example = {
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
 
-def unique_network_map(topology_dict):
-    new_dict = {}
-    result_unique_items_dict = {}
-    i = 0
-    for key,value in topology_dict.items():
-        if topology_dict.get(value) == key:
-            new_dict[key] = value
-        else:result_unique_items_dict[key] = value
-    for key , value in new_dict.items():
-        i += 1
-        if i <= int(len(new_dict)/2):
-            result_unique_items_dict[key] = value
-          
-    return result_unique_items_dict   
 
 class Topology:
     def __init__(self, topology_dict):
         self.topology = self._normalize(topology_dict)
         
     def _normalize(self, topology_dict):
-        return unique_network_map(topology_dict)
+        new_dict = {}
+        result_unique_items_dict = {}
+        i = 0
+        for key,value in topology_dict.items():
+            if topology_dict.get(value) == key:
+                new_dict[key] = value
+            else:result_unique_items_dict[key] = value
+        for key , value in new_dict.items():
+            i += 1
+            if i <= int(len(new_dict)/2):
+                result_unique_items_dict[key] = value
+              
+        return result_unique_items_dict 
         
-    
-
-
 
 
 if __name__ == '__main__':
