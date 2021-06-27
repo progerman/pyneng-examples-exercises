eaef107216d86b308d4ca7192e44f1be38d006a3
alex <aleksandr.telenchenko@gmail.com>
2021-06-27 09:35:37 -0500

    exec task_23_2.py test ok

--- exercises/23_oop_special_methods/task_23_2.py
+++ exercises/23_oop_special_methods/task_23_2.py
@@ -41,3 +41,49 @@ ValueError                                Traceback (most recent call last)
 
 ValueError: Возникла ошибка
 """
+from pprint import pprint
+import telnetlib
+
+
+class CiscoTelnet:
+    def __init__(self, **params):
+        self.params = params
+        ip, username, password, secret = params.values()
+        self.telnet = telnetlib.Telnet(ip) 
+        self.telnet.read_until(b'Username')
+        self.telnet.write(self._write_line(username))
+        self.telnet.read_until(b'Password')
+        self.telnet.write(self._write_line(password))
+        self.telnet.read_until(b'>')
+        self.telnet.write(b'enable\n')
+        self.telnet.read_until(b'Password')
+        self.telnet.write(self._write_line(secret))
+        self.telnet.read_until(b'#')
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.telnet.close()
+        
+    def _write_line(self,line):
+        return f"{line}\n".encode("utf-8")
+        
+    def send_show_command(self, command):
+        command_to_bite_encode = self._write_line(command)
+        self.telnet.write(command_to_bite_encode)
+        return self.telnet.read_until(b'#').decode('utf-8')
+
+
+if __name__ == '__main__':
+    r1_params = {
+            'ip': '192.168.100.1',
+            'username': 'cisco',
+            'password': 'cisco',
+            'secret': 'cisco'
+                }
+    #connect_telnet = CiscoTelnet(**r1_params)
+    with CiscoTelnet(**r1_params) as connect_telnet:
+        print(connect_telnet.send_show_command("sh ip int br"))
+    
+    
