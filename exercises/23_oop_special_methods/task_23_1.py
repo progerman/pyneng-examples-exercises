42f11d2bad61ff22719ef01fd500bd5143cb81f9
alex <aleksandr.telenchenko@gmail.com>
2021-06-27 07:02:27 -0500

    exec task_23_1.py test ok

--- exercises/23_oop_special_methods/task_23_1.py
+++ exercises/23_oop_special_methods/task_23_1.py
@@ -43,3 +43,62 @@ In [6]: ip1 = IPAddress('10.1.1.1/240')
 ValueError: Incorrect mask
 
 """
+
+
+class IPAddress:
+    
+    def __init__(self,ip_addr):
+        self.ip,self.mask = self._separete_ip_mask(ip_addr)
+        ip = self.ip
+        mask = self.mask
+        self._check_ip(ip)
+        self._check_mask(mask)
+    
+    def _separete_ip_mask(self, ip_addr):
+        ip = ip_addr.split('/')[0]
+        mask = ip_addr.split('/')[1]
+        return ip, int(mask)   
+        
+    def _check_ip(self , ip):
+        ip_addr = self.ip 
+        ip_addr2 = []
+        b =0
+        '------проверка длинны сообщения ------'
+        if 7 <= len(ip_addr) <= 15:
+            check_len_input=True
+        else:
+            raise ValueError(' Incorrect IPv4 address')
+        '-------Проверка цифр и точек----------------------'
+        for point in list(ip_addr):
+            if point.isdigit() == True:
+                ip_addr2.append(point)
+            elif point == '.': 
+                b=b+1
+                ip_addr2.append(point)
+            else: 
+                raise ValueError(' Incorrect IPv4 address')
+        '-----Проверка на точку в конце в начале и количество точек не равное 4м-----'
+        if ip_addr2[0] == '.' or ip_addr2[-1] == '.' or b != 3 : 
+            raise ValueError(' Incorrect IPv4 address')
+        for line in ip_addr.split('.'):
+            if  line.isdigit() and int(line) > 255:
+                raise ValueError(' Incorrect IPv4 address') 
+        return True
+        
+    def _check_mask(self, mask):
+        if 8 < mask > 32:
+            raise ValueError(' ValueError: Incorrect mask') 
+        return True
+            
+        
+if __name__ == '__main__':
+    ip = IPAddress('10.30.1.100/24')
+    print(ip.ip)
+    print(ip.mask)
+    
+    
+    
+    
+    
+    
+    
