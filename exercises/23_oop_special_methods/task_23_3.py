e70b6fc4257d2eb4bfb67dca5793fe849c667e5e
alex <aleksandr.telenchenko@gmail.com>
2021-06-27 11:17:49 -0500

    exec task_23_3.py test ok

--- exercises/23_oop_special_methods/task_23_3.py
+++ exercises/23_oop_special_methods/task_23_3.py
@@ -75,3 +75,67 @@ topology_example2 = {
     ("R1", "Eth0/4"): ("R7", "Eth0/0"),
     ("R1", "Eth0/6"): ("R9", "Eth0/0"),
 }
+
+
+from pprint import pprint
+
+#def unique_network_map(topology_dict):
+    #new_dict = {}
+    #result_unique_items_dict = {}
+    #i = 0
+    #for key,value in topology_dict.items():
+        #if topology_dict.get(value) == key:
+            #new_dict[key] = value
+        #else:result_unique_items_dict[key] = value
+    #for key , value in new_dict.items():
+        #i += 1
+        #if i <= int(len(new_dict)/2):
+            #result_unique_items_dict[key] = value
+          
+    #return result_unique_items_dict   
+
+
+class Topology:
+    
+    def __init__(self, topology):
+        self.topology = self._unique_network_map(topology)
+        
+    def __add__(self, other):
+        new_dict = self.topology.copy()
+        new_dict.update(other.topology)
+        return Topology(new_dict)
+        
+    def _unique_network_map(self,topology_dict):
+        new_dict = {}
+        result_unique_items_dict = {}
+        i = 0
+        for key,value in topology_dict.items():
+            if topology_dict.get(value) == key:
+                new_dict[key] = value
+            else:result_unique_items_dict[key] = value
+        for key , value in new_dict.items():
+            i += 1
+            if i <= int(len(new_dict)/2):
+                result_unique_items_dict[key] = value
+          
+        return result_unique_items_dict   
+    
+    
+
+
+if __name__ == '__main__':
+    top_1 = Topology(topology_example)
+    pprint(top_1.topology)
+    print(len(top_1.topology))
+    
+    top_2 = Topology(topology_example2)
+    pprint(top_2.topology)
+    print(len(top_2.topology))
+    
+    top3 = top_1+top_2
+    pprint(top3.topology)
+    print(len(top3.topology))
+    pprint(top_1.topology)
+
+
+
