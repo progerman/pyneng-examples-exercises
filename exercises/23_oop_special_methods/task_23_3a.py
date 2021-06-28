b0f195a45a8e1b5f62179f38c876cfddfaeb99ab
alex <aleksandr.telenchenko@gmail.com>
2021-06-28 02:38:14 -0500

    exec task_23_3a.py test ok

--- exercises/23_oop_special_methods/task_23_3a.py
+++ exercises/23_oop_special_methods/task_23_3a.py
@@ -41,3 +41,39 @@ topology_example = {
     ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
+
+
+from pprint import pprint
+
+def unique_network_map(topology_dict):
+    new_dict = {}
+    result_unique_items_dict = {}
+    i = 0
+    for key,value in topology_dict.items():
+        if topology_dict.get(value) == key:
+            new_dict[key] = value
+        else:result_unique_items_dict[key] = value
+    for key , value in new_dict.items():
+        i += 1
+        if i <= int(len(new_dict)/2):
+            result_unique_items_dict[key] = value
+          
+    return result_unique_items_dict   
+
+
+class Topology:
+    def __init__(self, topology):
+        
+        self.topology = unique_network_map(topology)
+    def __iter__(self):
+        return iter(self.topology.items())
+
+if __name__ == '__main__':
+    top = Topology(topology_example)
+    #pprint(top.topology)
+    for link in top:
+        print(link)
+    
+    
+    
+    
